ce022d0161951fbbc972a3e8746682a8427c3158
AkashItlogiko <akashsahanwu089@gmail.com>
2025-08-27 11:44:24 +0600

    Route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello',function(){
+   return "Hello World";
+});
+
+Route::get('/hello/from/jashore',function(){
+   return "Hello World-1";
+});
